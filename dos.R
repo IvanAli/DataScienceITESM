94f9409b7afa2bac2a56a541cd581ead45c05765
erikiado <erikiado@gmail.com>
2016-11-25 16:45:05 -0600

    Final commit?

--- dos.R
+++ dos.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 slim <- read.csv("slim_airplanes.csv")
 slim <- slim[2:14]
 
@@ -32,19 +34,35 @@ boxplot(slim$DestAirportID)
 title("Destination Airport ID")
 
 boxplot(slim$CRSDepTime)
-title("CRS Departure Time")
+title("CRS Departure Time",xlab="", ylab="HourMinute")
 
 boxplot(slim$DepDelay)
-title("Departure Delay")
+title("Departure Delay",xlab="", ylab="Minutes Difference")
 
 boxplot(slim$CRSArrTime)
-title("CRS Arrival Time")
+title("CRS Arrival Time",xlab="", ylab="HourMinute")
 
 boxplot(slim$ArrDelay)
-title("Arrival Delay")
+title("Arrival Delay",xlab="", ylab="Minutes Difference")
 
 boxplot(slim$Distance)
-title("Distance")
+title("Distance",xlab="", ylab="Kilometers")
 
 #boxplot(slim$CancellationCode)
 #boxplot(slim$Cancelled)
+
+qplot( y= ArrDelay,x = "Minutes Diference",data = subset(slim, !is.na(ArrDelay)), geom = 'boxplot')+coord_cartesian(ylim = c(-50, 50))
+qplot( y= ArrDelay, x = "Minutes Diference",data = subset(slim, !is.na(ArrDelay)),geom = 'boxplot')+coord_cartesian(ylim = c(-100, 50))
+qplot( y= DepDelay,x = CancellationCode,data = subset(slim, !is.na(DepDelay)), geom = 'boxplot')
+qplot( y= DepDelay,x = Cancplotelled,data = subset(slim, !is.na(Cancelled)), geom = 'boxplot')
+qplot( y= DepDelay,x = CancellationCode,data = subset(slim, !is.na(CancellationCode)), geom = 'boxplot')
+qplot( y= Month,x = CancellationCode,data = subset(slim, !is.na(CancellationCode)), geom = 'boxplot')
+qplot( y= DepDelay,x = UniqueCarrier,data = subset(slim, !is.na(UniqueCarrier)), geom = 'boxplot')+coord_cartesian(ylim = c(-50, 80))
+qplot( y= Distance,x = CancellationCode,data = subset(slim, !is.na(CancellationCode)), geom = 'boxplot')+geom_jitter(width = 0.2)
+qplot( y= DepDelay,x = Month,data = slim, geom = 'boxplot')
+
+
+qplot( y= DepDelay,x = Quarter,data = subset(slim, !is.na(Quarter)), geom = 'boxplot')
+
+qplot( y= DepDelay,x = as.factor(Quarter),data = subset(slim, !is.na(Quarter)), geom = 'boxplot')+coord_cartesian(ylim = c(-50, 80))
+
